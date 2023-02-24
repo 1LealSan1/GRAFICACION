13a3afdbb3612e370441318f0de7ece5a7565d1c
Leonel <73618352+1LealSan1@users.noreply.github.com>
2023-02-23 20:55:10 -0700

    tt

--- dda.js
+++ dda.js
@@ -1,41 +1,29 @@
 var canvas = document.getElementById('canvas');
 var preview = document.getElementById('preview')
 var context = preview.getContext('2d');
-var ctx = preview.getContext('2d');
-let startX, startY, endX, endY, option;
+var ctx = canvas.getContext('2d');
+
+let startX, startY, endX, endY, option, isDrawing=false;
 
 preview.addEventListener("mousedown", function(event){
   startX = event.offsetX;
   startY = event.offsetY;
+  isDrawing = true;
 });
 
 preview.addEventListener("mousemove", function(event) {
+  if (!isDrawing) return;
   endX = event.offsetX;
   endY = event.offsetY;
   limpiar();
-  if(startX!=null && startY!=null && endX!= null && endY != null ){
-    if(option == 'line'){
-      Bresenham(startX, startY, endX, endY, 0)
-    }else if(option =='circle'){
-      drawCircle(startX, startY, endX, endY)
-    }else if(option == 'square'){
-      drawSquare(startX, startY, endX, endY)
-    }else if(option == 'pentagon'){
-      drawPoligon(startX, startY, endX, endY, 5)
-    }else if(option == 'hexagon'){
-      drawPoligon(startX, startY, endX, endY, 6)
-    }else if(option == 'heptagon'){
-      drawPoligon(startX, startY, endX, endY, 7)
-    }else if(option == 'octagon'){
-      drawPoligon(startX, startY, endX, endY, 8)
-    }          
-  }
+  menu(0);
 });
 
 preview.addEventListener("mouseup",function(event){
+  isDrawing = false;
   endX = event.offsetX;
   endY = event.offsetY;
-  limpiar();
+  menu(1)
 });
 
 function opcion(op){
@@ -45,8 +33,27 @@ function opcion(op){
 function limpiar(){
   context.clearRect(0, 0, preview.width, preview.height);
 }
+function menu(l){
+  if(startX!=null && startY!=null && endX!= null && endY != null ){
+    if(option == 'line'){
+      Bresenham(startX, startY, endX, endY, l)
+    }else if(option =='circle'){
+      drawCircle(startX, startY, endX, endY,l)
+    }else if(option == 'square'){
+      drawSquare(startX, startY, endX, endY,l)
+    }else if(option == 'pentagon'){
+      drawPoligon(startX, startY, endX, endY, 5,l)
+    }else if(option == 'hexagon'){
+      drawPoligon(startX, startY, endX, endY, 6,l)
+    }else if(option == 'heptagon'){
+      drawPoligon(startX, startY, endX, endY, 7,l)
+    }else if(option == 'octagon'){
+      drawPoligon(startX, startY, endX, endY, 8,l)
+    }          
+  }
+}
 // DDA function
-function DDA(x1, y1, x2, y2) {
+/*function DDA(x1, y1, x2, y2) {
   let dx = Math.abs(x2 - x1);
   let dy = Math.abs(y2 - y1);
   let x = x1;
@@ -67,9 +74,9 @@ function DDA(x1, y1, x2, y2) {
           y += sy;
       }
   }
-}
+}*/
 
-function drawLine(startX, startY, endX, endY) {
+/*function drawLine(startX, startY, endX, endY) {
   let dx = Math.abs(endX - startX);
   let dy = Math.abs(endY - startY);
   let sx = (startX < endX) ? 1 : -1;
@@ -91,7 +98,7 @@ function drawLine(startX, startY, endX, endY) {
       startY = startY + sy;
       }
   }
-}
+}*/
 
 function Bresenham(startX, startY, endX, endY, l) {
   let dx = Math.abs(endX - startX);
@@ -103,8 +110,9 @@ function Bresenham(startX, startY, endX, endY, l) {
   while (true) {
     if(l== 0){
       context.fillRect(startX, startY, 1, 1);      
+    }else if(l==1){
+      ctx.fillRect(startX, startY, 1, 1)
     }
-    context.fillRect(startX, startY, 1, 1);
     if (startX === endX && startY === endY) break;
     let e2 = 2 * error;
     if (e2 > -dy) {
@@ -118,22 +126,24 @@ function Bresenham(startX, startY, endX, endY, l) {
   }
 }
 
-function drawSquare(x1, y1, x2, y2) {
-  //Primera linea base
-  Bresenham(x1, y1, x2, y2)
+function drawSquare(startX, startY, endX, endY, l) {
+  var width = Math.abs(endX - startX);
+  var height = Math.abs(endY - startY);
+  var squareSize = Math.max(width, height);
 
-  //segunda linea base
-  dyx = y1 + Math.abs(x2 - x1);
-  Bresenham(x1, y1, x1, dyx)
+  var centerX = (startX + endX) / 2;
+  var centerY = (startY + endY) / 2;
 
-  //tercera linea base
-  Bresenham(x1, dyx, x2, dyx)
-
-  //cuarta linea base
-  DDA(x2, dyx, x2, y2)
+  var squareX = centerX - squareSize / 2;
+  var squareY = centerY - squareSize / 2;
+  
+  Bresenham(squareX, squareY, squareX + squareSize, squareY, l);
+  Bresenham(squareX + squareSize, squareY, squareX + squareSize, squareY + squareSize, l);
+  Bresenham(squareX + squareSize, squareY + squareSize, squareX, squareY + squareSize, l);
+  Bresenham(squareX, squareY + squareSize, squareX, squareY, l);
 }
 
-function drawCircle(startX, startY, endX, endY) {
+function drawCircle(startX, startY, endX, endY, l) {
   let x0 = (startX + endX) / 2;
   let y0 = (startY + endY) / 2;
   radius  = Math.max(Math.abs(startX - endX), Math.abs(startY - endY)) / 2;
@@ -142,14 +152,25 @@ function drawCircle(startX, startY, endX, endY) {
   let decisionOver2 = 1 - x;
 
   while (y <= x) {
-    context.fillRect(x + x0, y + y0, 1, 1);
-    context.fillRect(y + x0, x + y0, 1, 1);
-    context.fillRect(-x + x0, y + y0, 1, 1);
-    context.fillRect(-y + x0, x + y0, 1, 1);
-    context.fillRect(-x + x0, -y + y0, 1, 1);
-    context.fillRect(-y + x0, -x + y0, 1, 1);
-    context.fillRect(x + x0, -y + y0, 1, 1);
-    context.fillRect(y + x0, -x + y0, 1, 1);
+    if(l==0){
+      context.fillRect(x + x0, y + y0, 1, 1);
+      context.fillRect(y + x0, x + y0, 1, 1);
+      context.fillRect(-x + x0, y + y0, 1, 1);
+      context.fillRect(-y + x0, x + y0, 1, 1);
+      context.fillRect(-x + x0, -y + y0, 1, 1);
+      context.fillRect(-y + x0, -x + y0, 1, 1);
+      context.fillRect(x + x0, -y + y0, 1, 1);
+      context.fillRect(y + x0, -x + y0, 1, 1);
+    }else if(l==1){
+      ctx.fillRect(x + x0, y + y0, 1, 1);
+      ctx.fillRect(y + x0, x + y0, 1, 1);
+      ctx.fillRect(-x + x0, y + y0, 1, 1);
+      ctx.fillRect(-y + x0, x + y0, 1, 1);
+      ctx.fillRect(-x + x0, -y + y0, 1, 1);
+      ctx.fillRect(-y + x0, -x + y0, 1, 1);
+      ctx.fillRect(x + x0, -y + y0, 1, 1);
+      ctx.fillRect(y + x0, -x + y0, 1, 1);
+    }
     y++;
     if (decisionOver2 <= 0) {
       decisionOver2 += 2 * y + 1;
@@ -160,7 +181,7 @@ function drawCircle(startX, startY, endX, endY) {
   }
 }
 
-function drawPoligon2(centerX, centerY, xCoords, yCoords, sides){
+function drawPoligon2(centerX, centerY, xCoords, yCoords, sides, l){
   for (let i = 0; i < sides; i++) {
     const angle = (i * 2 * Math.PI) / sides;
     const x = centerX + radius * Math.cos(angle);
@@ -184,25 +205,38 @@ function drawPoligon2(centerX, centerY, xCoords, yCoords, sides){
 
     let x = x1;
     let y = y1;
-
-    context.beginPath();
-    context.moveTo(x, y);
-
-    for (let j = 0; j < steps; j++) {
-      x += xIncrement;
-      y += yIncrement;
-      context.lineTo(x, y);
+    if(l==0){
+      context.beginPath();
+      context.moveTo(x, y);
+  
+      for (let j = 0; j < steps; j++) {
+        x += xIncrement;
+        y += yIncrement;
+        context.lineTo(x, y);
+      }
+  
+      context.stroke();
+    }else if(l==1){
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+  
+      for (let j = 0; j < steps; j++) {
+        x += xIncrement;
+        y += yIncrement;
+        ctx.lineTo(x, y);
+      }
+  
+      ctx.stroke();
     }
 
-    context.stroke();
   }
 }
 
-function drawPoligon(startX, startY, endX, endY, sides){
+function drawPoligon(startX, startY, endX, endY, sides, l){
   let centerX = (startX + endX) / 2;
   let centerY = (startY + endY) / 2;
   let xCoords = [], yCoords = [];
   radius  = Math.max(Math.abs(startX - endX), Math.abs(startY - endY)) / 2;
 
-  drawPoligon2(centerX, centerY, xCoords, yCoords, sides)
+  drawPoligon2(centerX, centerY, xCoords, yCoords, sides, l)
 }
