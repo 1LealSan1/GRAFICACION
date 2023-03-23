27326747fb066dfe8060d35e3dc363fbbd019885
Leonel <73618352+1LealSan1@users.noreply.github.com>
2023-03-24 00:38:12 +0100

    ff

--- p.js
+++ p.js
@@ -75,20 +75,8 @@ function menu(x1, y1 , x2 , y2, opt, c){
   if(x1!=null && y1!=null && x2!= null && y2 != null ){
     if(opt == 'line'){
         Bresenham(x1, y1 , x2 , y2, c)
-      }else if(opt =='circle'){
-        drawCircle(x1, y1 , x2 , y2, c)
-      }else if(opt == 'square'){
-        drawSquare(x1, y1 , x2 , y2, c)
-      }else if(opt == 'pentagon'){
-        drawPoligon(x1, y1 , x2 , y2, 5,c)
-      }else if(opt == 'hexagon'){
-        drawPoligon(x1, y1 , x2 , y2, 6,c)
-      }else if(opt == 'heptagon'){
-        drawPoligon(x1, y1 , x2 , y2, 7,c)
-      }else if(opt == 'octagon'){
-        drawPoligon(x1, y1 , x2 , y2, 8,c)
-      } 
-  }
+      }
+    }
 }
 
 function Bresenham(startX, startY, endX, endY, c) {
@@ -112,94 +100,4 @@ function Bresenham(startX, startY, endX, endY, c) {
       startY += sy;
     }
   }
-}
-
-function drawSquare(startX, startY, endX, endY, c) {
-  var width = Math.abs(endX - startX);
-  var height = Math.abs(endY - startY);
-  var squareSize = Math.max(width, height);
-
-  var centerX = (startX + endX) / 2;
-  var centerY = (startY + endY) / 2;
-
-  var squareX = centerX - squareSize / 2;
-  var squareY = centerY - squareSize / 2;
-  Bresenham(squareX, squareY, squareX + squareSize, squareY, c);
-  Bresenham(squareX + squareSize, squareY, squareX + squareSize, squareY + squareSize, c);
-  Bresenham(squareX + squareSize, squareY + squareSize, squareX, squareY + squareSize, c);
-  Bresenham(squareX, squareY + squareSize, squareX, squareY, c);
-}
-
-function drawCircle(startX, startY, endX, endY, c) {
-  let x0 = (startX + endX) / 2;
-  let y0 = (startY + endY) / 2;
-  radius  = Math.max(Math.abs(startX - endX), Math.abs(startY - endY)) / 2;
-  let x = radius;
-  let y = 0;
-  let decisionOver2 = 1 - x;
-
-  while (y <= x) {
-    ctx.fillStyle=String(c);
-    ctx.fillRect(x + x0, y + y0, 1, 1);
-    ctx.fillRect(y + x0, x + y0, 1, 1);
-    ctx.fillRect(-x + x0, y + y0, 1, 1);
-    ctx.fillRect(-y + x0, x + y0, 1, 1);
-    ctx.fillRect(-x + x0, -y + y0, 1, 1);
-    ctx.fillRect(-y + x0, -x + y0, 1, 1);
-    ctx.fillRect(x + x0, -y + y0, 1, 1);
-    ctx.fillRect(y + x0, -x + y0, 1, 1);
-
-    y++;
-    if (decisionOver2 <= 0) {
-      decisionOver2 += 2 * y + 1;
-    } else {
-      x--;
-      decisionOver2 += 2 * (y - x) + 1;
-    }
-  }
-}
-
-function drawPoligon2(centerX, centerY, xCoords, yCoords, sides, c){
-  for (let i = 0; i < sides; i++) {
-    const angle = (i * 2 * Math.PI) / sides;
-    const x = centerX + radius * Math.cos(angle);
-    const y = centerY + radius * Math.sin(angle);
-    xCoords.push(x);
-    yCoords.push(y);
-  }
-  for (let i = 0; i < sides; i++) {
-    let x1 = xCoords[i];
-    let y1 = yCoords[i];
-    let x2 = xCoords[(i + 1) % sides];
-    let y2 = yCoords[(i + 1) % sides];
-
-    let dx = x2 - x1;
-    let dy = y2 - y1;
-    let steps = Math.max(Math.abs(dx), Math.abs(dy));
-
-    let xIncrement = dx / steps;
-    let yIncrement = dy / steps;
-
-    let x = x1;
-    let y = y1;
-    ctx.strokeStyle = String(c)
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-  
-    for (let j = 0; j < steps; j++) {
-        x += xIncrement;
-        y += yIncrement;
-        ctx.lineTo(x, y);
-    }
-    ctx.stroke();
-  }
-}
-
-function drawPoligon(startX, startY, endX, endY, sides, c){
-  let centerX = (startX + endX) / 2;
-  let centerY = (startY + endY) / 2;
-  let xCoords = [], yCoords = [];
-  radius  = Math.max(Math.abs(startX - endX), Math.abs(startY - endY)) / 2;
-
-  drawPoligon2(centerX, centerY, xCoords, yCoords, sides, c)
 }
